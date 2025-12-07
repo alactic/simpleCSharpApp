37b2a380c6d15ddf80ef96c687175eac48548f64
alactic <okaforechezona1992@gmail.com>
2025-12-07 09:42:39 +0000

    login endpoint implementation

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -7,6 +7,7 @@ using api.Interfaces;
 using api.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace api.Controllers
 {
@@ -16,10 +17,12 @@ namespace api.Controllers
     {
         private readonly UserManager<AppUser> _userManger;
         private readonly ITokenService _tokenService;
-         public AccountController(UserManager<AppUser> userManager, ITokenService tokenService)
+        private readonly SignInManager<AppUser> _signInManager;
+         public AccountController(UserManager<AppUser> userManager, ITokenService tokenService, SignInManager<AppUser> signInManager)
         {
             _userManger = userManager; 
             _tokenService = tokenService;
+            _signInManager = signInManager;
         }
 
         [HttpPost("register")]
@@ -65,6 +68,30 @@ namespace api.Controllers
             {
                 return StatusCode(500, e.Message);
             }
+        }
+
+        [HttpPost("/login")]
+        public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var user = await _userManger.Users.FirstOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
+            if(user == null) return Unauthorized("Invalid Username");
+            
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+            
+            if(!result.Succeeded) return Unauthorized("Username not found and/or password not found");
+
+            return Ok(
+                new NewUserDto
+                {
+                    UserName = user.UserName,
+                    Email = user.Email,
+                    Token = _tokenService.CreateToken(user),
+                }
+            );
         }
 
     }
--- obj/Debug/net10.0/api.AssemblyInfo.cs
+++ obj/Debug/net10.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb01a02cdced32866796f7645b7bde4f74b4b5ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d594714775ad409446947129b5510097f2ff4c7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
